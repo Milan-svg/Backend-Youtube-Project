8261b3c1a556591fcf3b6b00c16c2b9bc6eec740
Milan-svg <milampanchal@gmail.com>
2024-09-11 20:31:14 +0530

    added routes for user management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser, refreshAccessToken} from '../controllers/user.controller.js'
+import {loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar ,updateUserCoverImage, getUserChannelProfile, getWatchHistory } from '../controllers/user.controller.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +24,14 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar") ,updateUserAvatar)
+router.route("/update-coverimage").patch(verifyJWT, upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
 
 
 //app.use("/api/v1/users", userRouter) use krke hum yaha aagye, now //users/register ke liye we have this. registerUser is a controller we defined in user.controller file.
