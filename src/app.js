53e1091ec978a1a29e7cf344bc287522a099c1dd
Milan-svg <milampanchal@gmail.com>
2024-09-17 00:52:37 +0530

    simple healthcheck endpoint added

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ import commentRouter from './routes/comment.routes.js';
 import likeRouter from './routes/like.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import dashboardRouter from './routes/dashboard.routes.js';
+import healthcheckRouter from './routes/healthcheck.routes.js';
 
 //Routes Declaration
 
@@ -38,6 +39,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 
 
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,16 @@
+import {ApiError} from '../utils/apiError.js'
+import { ApiResponse } from "../utils/apiResponse.js";
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.status(200).json(
+        new ApiResponse(200, { status:"OK", message: "server is running"}, "healthcheck successfull")
+    )
+})
+
+export {
+    healthcheck
+    }
+    
